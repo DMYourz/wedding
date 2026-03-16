526575381fb7069fa9d305978fb6d16044134256
Daniel Mochkarovsky <dmyourz@gmail.com>
2026-03-15 21:16:08 -0400

    Fix base path for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-// If your repo is named "wedding" the site will be at: yourusername.github.io/wedding
-// Change the base below to match your repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/wedding/',
 })
